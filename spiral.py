3e34c4c12338c758ccf57e14c77ab300cc053f85
the-repo-hub <87334372+the-repo-hub@users.noreply.github.com>
2023-03-31 23:01:14 +0300

    Update spiral.py

--- spiral.py
+++ spiral.py
@@ -1,23 +1,23 @@
-N = -1
-matrix = [[0 for i in range(N)] for j in range(N)]
+y, x = map(int, input().split())
+matrix = [[0 for i in range(x)] for j in range(y)]
 counter = 1
 a, b = 0, 0
-num = N - 1
-pseudo_zero = 0
-while counter <= N*N:
+iteration = 0
+num_x, num_y = x - 1, y - 1
+while counter <= x*y:
 
     matrix[a][b] = counter
     counter += 1
 
-    if b < num - pseudo_zero and a == pseudo_zero: # right
+    if a == iteration and b < num_x - iteration:
         b += 1
-    elif b == num - pseudo_zero and a < num - pseudo_zero: # down
+    elif b == num_x - iteration and a < num_y - iteration:
         a += 1
-    elif b != pseudo_zero and a == num - pseudo_zero: # left
+    elif a == num_y - iteration and b != iteration:
         b -= 1
-    else: # up
+    else:
         a -= 1
-        if a - 1 == pseudo_zero:
-            pseudo_zero += 1
+        if a - 1 == iteration:
+            iteration += 1
 for line in matrix:
     print(*line)
